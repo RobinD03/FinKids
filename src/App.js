baf662fe8ea3a928db7b5aeec7a3a82de994c157
Thomasahn8 <thomasahn@tamu.edu>
2023-01-28 14:39:38 -0600

    d

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Robin
+          Learn chochothomas
         </a>
       </header>
     </div>
