0b348580d096ffa6c1db8f2e4630ec6a8ee4e2a3
Thomasahn8 <thomasahn@tamu.edu>
2023-01-28 14:45:57 -0600

    lokesh f

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn chochothomas
+          Learn chochorobin lokesh
         </a>
       </header>
     </div>
