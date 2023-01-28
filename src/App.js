e09c99f128ed14a373f00c0cf946dfe0924cd8db
RobinD03 <robindalela@gmail.com>
2023-01-28 14:52:30 -0600

    Reverted back to original

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn chochorobin lokesh
-          {/* hey there dingus the chingus*/}
+          Learn Robin
         </a>
       </header>
     </div>
